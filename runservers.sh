bc1a38bdf4c515711a2412eae86ffebd28f05f80
Alex Archer <alex.archer@okta.com>
2022-05-20 18:27:45 -0400

    add gitignore

--- runservers.sh
+++ runservers.sh
@@ -4,19 +4,15 @@ echo
 All SERVERS STARTING...
 <<<<<<<<<<<<<<<"
 
-
-# open all localhost ports in browser
-open http://localhost:4040
-open http://localhost:8080
-open http://localhost:3000
-open http://localhost:8666
-
-
-# Okta Sign In Widget
+# Okta Sign In Widget (Vue)
 ttab eval "cd ~/Documents/CustomApps/aarcher-spa-vue-app-okta-sign-in-upgraded 
 
 npm run serve"
 
+#Okta Sign In Widget (React)
+ttab eval "cd ~/Documents/CustomApps/aarcher-react-spa-okta-authentication
+
+npm start"
 
 #Auth0 Simple Redirect SPA
 ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/02-calling-an-api 
@@ -27,9 +23,9 @@ npm run serve"
 # Auth0 Pug JS Web App 
 
 # Node.js
-ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
+# ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
 
-npm run dev"
+# npm run dev"
 
 # Front End
 ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
@@ -49,3 +45,12 @@ python3 -m venv venv
 source venv/bin/activate
 
 python3 app.py"
+
+
+# open all localhost ports in browser
+# open http://localhost:4040
+open https://localhost:3030
+open http://localhost:8080
+open http://localhost:3000
+open http://localhost:8666
+
