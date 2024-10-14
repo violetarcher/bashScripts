b6feeb7311c8ea41e6fe589f446c0b1b07f0874b
Alex Archer <alex.archer@okta.com>
2024-10-14 17:21:40 -0400

    add new web app

--- runservers.sh
+++ runservers.sh
@@ -5,24 +5,24 @@ All SERVERS STARTING...
 <<<<<<<<<<<<<<<"
 
 # Okta Sign In Widget (Vue) - port 8080
-ttab eval "cd ~/Documents/CustomApps/aarcher-spa-vue-app-okta-sign-in-upgraded 
+# ttab eval "cd ~/Documents/CustomApps/aarcher-spa-vue-app-okta-sign-in-upgraded 
 
-npm run serve"
+# npm run serve"
 
 #Okta Sign In Widget (React) - port 3030
-ttab eval "cd ~/Documents/CustomApps/aarcher-react-spa-okta-authentication
+# ttab eval "cd ~/Documents/CustomApps/aarcher-react-spa-okta-authentication
 
-npm start"
+# npm start"
 
 #Okta Redirect App with Dashboard SSO (for B2B SSO) (React) - port 5001
-ttab eval "cd ~/Documents/CustomApps/okta-react-quickstart
+# ttab eval "cd ~/Documents/CustomApps/okta-react-quickstart
 
-npm start"
+# npm start"
 
 #Okta Sample Redirect App with Resource server message and Profile (for B2B SSO) (React) - port 5050
-ttab eval "cd ~/Documents/CustomApps/samples-js-react/okta-hosted-login
+# ttab eval "cd ~/Documents/CustomApps/samples-js-react/okta-hosted-login
 
-npm start"
+# npm start"
 
 #Auth0 Simple Redirect SPA (Vue) - port 3000
 # ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/02-calling-an-api 
@@ -30,9 +30,9 @@ npm start"
 # npm run serve"
 
 #Auth0 React Redirect with custom Branding template & domain app - port 3002
-ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/sample-01
+# ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/sample-01
 
-npm run dev"
+# npm run dev"
 
 #Auth0 React with Healthcare Theme
 ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/react-progressive-profiling
@@ -45,22 +45,25 @@ ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/authorization-api-r
 npm start"
 
 
-# Auth0 Pug JS Web App - localhost 4040
-
-# Node.js
-ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
+# Auth0 Next JS Web App - localhost 4040, API 4001
+ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/2024-web-app-next-js
 
 npm run dev"
 
-# Front End
-ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
+# Node.js
+# ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/
 
-npm run ui"
+# npm run dev"
+
+# Front End
+# ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-pug-sample 
+
+# npm run ui"
 
 # Serve API
-ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-js-sample
+# ttab eval "cd ~/Documents/Auth0CustomApps/PublicCloudTenants/auth0-express-node-pug-app/auth0-express-js-sample
 
-npm start"
+# npm start"
 
 # Start Admin demo from Zartan - port 8666
 # ttab eval "cd ~/Documents/zartan/zartan
@@ -74,9 +77,9 @@ npm start"
 # open all localhost ports in browser
 # open http://localhost:4040
 open https://localhost:3030
-open http://localhost:8080
+# open http://localhost:8080
 open http://localhost:3000
 # open http://localhost:8666
-open http://localhost:3002
-open http://localhost:5001
+# open http://localhost:3002
+# open http://localhost:5001
 
